c8c2e409304199a253b14ddf9befd8385168458a
Axell Solis <axellsolisrubio@gmail.com>
2022-04-29 12:49:05 -0500

    Add styles using tailwind

--- storedjango/store/models.py
+++ storedjango/store/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=150)
     price = models.FloatField()
     description = models.TextField()
+    image = models.ImageField(blank=True, upload_to="images")
 
     def __str__(self):
         return self.name
