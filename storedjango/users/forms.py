19a543e038eb250dbe2802e7fb451e8389ae68f7
Axell Solis <axellsolisrubio@gmail.com>
2022-05-03 12:57:10 -0500

    Add user authenticaction, register and login templates create

---
+++ storedjango/users/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "outline-0 outline-none p-2",
+                "placeholder": "email@example.com",
+            }
+        ),
+    )
+    username = forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={"class": "outline-none outline-0 p-2"}),
+    )
+    password1 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={"class": "otuline-none outline-0 p-2"}),
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={"class": "otuline-none outline-0 p-2"}),
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user_email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
--- storedjango/users/views.py
+++ storedjango/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.urls import reverse
+from .forms import NewUserForm
 
-# Create your views here.
+
+def register(request: HttpRequest):
+    if request.method == "POST":
+
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(reverse("store:products"))
+
+    form = NewUserForm()
+    context = {"form": form}
+    return render(request, "users/register.html", context)
